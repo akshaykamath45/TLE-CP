8e683a3dec7d86e9ef0b9836ab88b3941343c998
Akshay Kamath <73344382+akshaykamath45@users.noreply.github.com>
2022-12-15 04:32:50 +0530

    Max(D3 P4)
    Given a number N, and N numbers, find maximum number in these N numbers.
    
    Link:https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/E

---
+++ LEVEL 1/C++ Basics/PROBLEMS/3.4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    int a[N];
+    for(int i=0;i<N;i++){
+        cin>>a[i];
+    }
+    int max;
+    max=a[0];
+    for(int i=0;i<N;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+    }
+    cout<<max<<endl;
+    
+}
