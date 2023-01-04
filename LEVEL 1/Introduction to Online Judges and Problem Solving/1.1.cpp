461d5d321dd17ddf97e43c0498eb1e3daab0742d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-04 23:27:27 +0530

    Middle Letter(D1 P4)
    Problem Statement
    You are given an odd-length string S consisting of lowercase English letters.
    
    Print the central character of S.
    
    Constraints
    S is an odd-length string consisting of lowercase English letters.
    The length of S is between 1 and 99 (inclusive).
    
    Input
    Input is given from Standard Input in the following format:
    S
    
    Output
    Print the answer.
    
    Link:https://atcoder.jp/contests/abc266/tasks/abc266_a

---
+++ LEVEL 1/Introduction to Online Judges and Problem Solving/1.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s;
+  cin>>s;
+  int n=s.size();
+  int mid=(n+1)/2;
+  cout<<s[mid-1];
+
+    return 0;
+}
