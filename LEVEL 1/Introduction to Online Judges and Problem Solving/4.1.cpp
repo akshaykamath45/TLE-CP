a060e90fc4d5792e7850cc308251331792498c8c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-07 14:54:45 +0530

    Balanced Array(D4 P1)
    You are given a positive integer n, it is guaranteed that n is even (i.e. divisible by 2).
    
    You want to construct the array a of length n such that:
    
    The first n2 elements of a are even (divisible by 2);
    the second n2 elements of a are odd (not divisible by 2);
    all elements of a are distinct and positive;
    the sum of the first half equals to the sum of the second half (∑i=1n2ai=∑i=n2+1nai).
    If there are multiple answers, you can print any. It is not guaranteed that the answer exists.
    
    You have to answer t independent test cases.
    
    Input
    The first line of the input contains one integer t (1≤t≤104) — the number of test cases. Then t test cases follow.
    
    The only line of the test case contains one integer n (2≤n≤2⋅105) — the length of the array. It is guaranteed that that n is even (i.e. divisible by 2).
    
    It is guaranteed that the sum of n over all test cases does not exceed 2⋅105 (∑n≤2⋅105).
    
    Output
    For each test case, print the answer — "NO" (without quotes), if there is no suitable answer for the given test case or "YES" in the first line and any suitable array a1,a2,…,an (1≤ai≤109) satisfying conditions from the problem statement on the second line.
    
    Link:https://codeforces.com/problemset/problem/1343/B

---
+++ LEVEL 1/Introduction to Online Judges and Problem Solving/4.1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+    
+    //If n/2 is odd,it is not possible,so we have to print "NO",since sum would be dd
+    //last odd element ,we can add   addition of n/2
+    int n;
+    cin>>n;
+    if((n/2)%2!=0){
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES"<<endl;;
+        for(int i=1;i<=n/2;i++){
+            cout<<2*i<<' ';//Printing even numbers first
+        }
+        for(int i=1;i<n/2;i++){
+            cout<<2*i-1<<' ';//Printing odd numbers
+        }
+        cout<<3*(n/2)-1<<'\n';//Very very important,For last odd digit
+    }
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
