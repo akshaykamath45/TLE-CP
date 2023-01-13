7fecaa3a29acf86d53927837bee6dba3d2b61760
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-14 01:58:38 +0530

    Hexadecimal's theorem(D4 P1)
    Recently, a chaotic virus Hexadecimal advanced a new theorem which will shake the Universe. She thinks that each Fibonacci number can be represented as sum of three not necessary different Fibonacci numbers.
    
    Let's remember how Fibonacci numbers can be calculated. F0 = 0, F1 = 1, and all the next numbers are Fi = Fi - 2 + Fi - 1.
    
    So, Fibonacci numbers make a sequence of numbers: 0, 1, 1, 2, 3, 5, 8, 13, ...
    
    If you haven't run away from the PC in fear, you have to help the virus. Your task is to divide given Fibonacci number n by three not necessary different Fibonacci numbers or say that it is impossible.
    
    Input
    The input contains of a single integer n (0 ≤ n < 109) — the number that should be represented by the rules described above. It is guaranteed that n is a Fibonacci number.
    
    Output
    Output three required numbers: a, b and c. If there is no answer for the test you have to print "I'm too stupid to solve this problem" without the quotes.
    
    If there are multiple answers, print any of them.
    
    Link:https://codeforces.com/problemset/problem/199/A

---
+++ LEVEL 1/Time and Space Complexity/4.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+//   int fib0=0;
+//   int fib1=1;
+  cout<<0<<' '<<0<<' '<<n<<endl;//Since 0 is a fibonacci number(given in the question).
+  
+    return 0;
+  
+}
